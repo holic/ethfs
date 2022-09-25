d579fdaa9b9f3a47f449f64df22f0bc69b70d812
Kevin Ingersoll <kingersoll@gmail.com>
2022-09-24 20:45:34 -0700

    switch to assembly for more read gas savings

--- packages/contracts/src/File.sol
+++ packages/contracts/src/File.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.13;
 
 import {SSTORE2} from "solady/utils/SSTORE2.sol";
-import {DynamicBuffer} from "ethier/contracts/utils/DynamicBuffer.sol";
 
 struct Content {
     bytes32 checksum;
@@ -14,12 +13,37 @@ struct File {
     Content[] contents;
 }
 
-function read(File memory file) view returns (string memory) {
-    bytes memory data = DynamicBuffer.allocate(file.size);
+function read(File memory file) view returns (string memory contents) {
+    address[] memory pointers = new address[](file.contents.length);
     for (uint256 i = 0; i < file.contents.length; i++) {
-        DynamicBuffer.appendSafe(data, SSTORE2.read(file.contents[i].pointer));
+        pointers[i] = file.contents[i].pointer;
     }
-    return string(data);
+
+    // Adapted from https://gist.github.com/xtremetom/20411eb126aaf35f98c8a8ffa00123cd
+    assembly {
+        let len := mload(pointers)
+        let totalSize := 0x20
+        let size := 0
+        contents := mload(0x40)
+
+        // loop through all pointer addresses
+        // - get address
+        // - get data size
+        // - get code and add to contents
+        // - update total size
+        let pointer := 0
+        for { let i := 0 } lt(i, len) { i := add(i, 1) } {
+            pointer := mload(add(pointers, add(0x20, mul(i, 0x20))))
+            size := sub(extcodesize(pointer), 1)
+            extcodecopy(pointer, add(contents, totalSize), 1, size)
+            totalSize := add(totalSize, size)
+        }
+
+        // update contents size
+        mstore(contents, sub(totalSize, 0x20))
+        // store contents
+        mstore(0x40, add(contents, and(add(totalSize, 0x1f), not(0x1f))))
+    }
 }
 
 using {
