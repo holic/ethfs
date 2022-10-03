d1fd4137fb9613ac2b413466ea76a09d977a9531
Kevin Ingersoll <kingersoll@gmail.com>
2022-10-03 08:47:46 -0700

    turn off alchemy provider for now

--- packages/app/src/EthereumProviders.tsx
+++ packages/app/src/EthereumProviders.tsx
@@ -24,7 +24,7 @@ const targetChains = defaultChains.filter(
 export const { chains, provider, webSocketProvider } = configureChains(
   targetChains,
   [
-    alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY }),
+    // alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY }),
     publicProvider(),
   ]
 );
