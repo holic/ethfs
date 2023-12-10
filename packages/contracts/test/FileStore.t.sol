c989420fc3a0f1a71b8ec2ca675b0dd3cd25bd2b
Kevin Ingersoll <kingersoll@gmail.com>
2023-12-10 08:59:40 -0800

    add gas report

--- packages/contracts/test/FileStore.t.sol
+++ packages/contracts/test/FileStore.t.sol
@@ -2,13 +2,14 @@
 pragma solidity >=0.8.10 <0.9.0;
 
 import "forge-std/Test.sol";
+import {GasReporter} from "@latticexyz/gas-report/GasReporter.sol";
 import {IContentStore} from "../src/IContentStore.sol";
 import {ContentStore} from "../src/ContentStore.sol";
 import {IFileStore} from "../src/IFileStore.sol";
 import {FileStore} from "../src/FileStore.sol";
 import {File} from "../src/File.sol";
 
-contract FileStoreTest is Test {
+contract FileStoreTest is Test, GasReporter {
     IContentStore public contentStore;
     IFileStore public fileStore;
 
@@ -32,44 +33,11 @@ contract FileStoreTest is Test {
             0x4e59b44847b379578588920cA78FbF26c0B4956C
         );
         fileStore = new FileStore(contentStore);
-
-        address pointer = fileStore.contentStore().addContent(
-            bytes(vm.readFile("test/files/sstore2-max.txt"))
-        );
-
-        address[] memory pointers = new address[](4);
-        pointers[0] = pointer;
-        pointers[1] = pointer;
-        pointers[2] = pointer;
-        pointers[3] = pointer;
-
-        uint256 startGas = gasleft();
-        (address bigFilePointer, ) = fileStore.createFile("big.txt", pointers);
-        console.log("FileStore.createFile gas", startGas - gasleft());
-        console.log("created big.txt file", bigFilePointer);
-    }
-
-    function testGetFile() public {
-        vm.expectRevert(
-            abi.encodeWithSelector(
-                IFileStore.FileNotFound.selector,
-                "non-existent.txt"
-            )
-        );
-        fileStore.getFile("non-existent.txt");
-
-        File memory file = fileStore.getFile("big.txt");
-
-        assertEq(file.size, 98300);
-        assertEq(bytes(file.read()).length, 98300);
     }
 
     function testCreateFile() public {
         string memory contents = vm.readFile("test/files/sstore2-max.txt");
-        address pointer = fileStore.contentStore().pointerForContent(
-            bytes(contents)
-        );
-
+        address pointer = fileStore.contentStore().addContent(bytes(contents));
         address[] memory pointers = new address[](1);
         pointers[0] = pointer;
 
@@ -82,7 +50,9 @@ contract FileStoreTest is Test {
             new bytes(0)
         );
 
+        startGasReport("create 24kb file");
         (, File memory file) = fileStore.createFile("24kb.txt", pointers);
+        endGasReport();
 
         assertEq(file.size, 24575);
         assertEq(bytes(file.read()).length, 24575);
@@ -117,22 +87,61 @@ contract FileStoreTest is Test {
     }
 
     function testDeleteFile() public {
+        address pointer = fileStore.contentStore().addContent(
+            bytes("hello world")
+        );
+        address[] memory pointers = new address[](1);
+        pointers[0] = pointer;
+        fileStore.createFile("hello.txt", pointers);
+
         assertTrue(
-            fileStore.fileExists("big.txt"),
-            "expected file big.txt to exist"
+            fileStore.fileExists("hello.txt"),
+            "expected file hello.txt to exist"
         );
 
         vm.expectEmit(true, true, true, true);
         emit FileDeleted(
-            "big.txt",
-            address(0x77D0d321c8bdD4C3D4cC7692252aCa06a292902B),
-            "big.txt"
+            "hello.txt",
+            address(0xF44764Ce40B56EdF2788f159D78224F59dC0C839),
+            "hello.txt"
         );
 
-        fileStore.deleteFile("big.txt");
+        fileStore.deleteFile("hello.txt");
         assertFalse(
-            fileStore.fileExists("big.txt"),
-            "expected file big.txt to no longer exist"
+            fileStore.fileExists("hello.txt"),
+            "expected file hello.txt to no longer exist"
         );
+    }
+
+    function testBigFile() public {
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IFileStore.FileNotFound.selector,
+                "non-existent.txt"
+            )
+        );
+        fileStore.getFile("non-existent.txt");
+
+        address pointer = fileStore.contentStore().addContent(
+            bytes(vm.readFile("test/files/sstore2-max.txt"))
+        );
+
+        address[] memory pointers = new address[](4);
+        pointers[0] = pointer;
+        pointers[1] = pointer;
+        pointers[2] = pointer;
+        pointers[3] = pointer;
+
+        startGasReport("create big file");
+        fileStore.createFile("big.txt", pointers);
+        endGasReport();
+
+        File memory file = fileStore.getFile("big.txt");
+        startGasReport("read big file");
+        file.read();
+        endGasReport();
+
+        assertEq(file.size, 98300);
+        assertEq(bytes(file.read()).length, 98300);
     }
 }
