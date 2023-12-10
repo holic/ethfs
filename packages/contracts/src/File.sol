5647e39855b93ea0d5ef7814145d91b6f4b41240
Kevin Ingersoll <kingersoll@gmail.com>
2023-12-10 10:42:34 -0800

    rewrite file.read in assembly

--- packages/contracts/src/File.sol
+++ packages/contracts/src/File.sol
@@ -15,23 +15,50 @@ struct File {
     BytecodeSlice[] slices;
 }
 
-function read(File memory file) view returns (string memory) {
+function read(File memory file) view returns (string memory contents) {
     BytecodeSlice[] memory slices = file.slices;
 
-    // TODO: rewrite this in assembly?
-    bytes memory buffer = DynamicBuffer.allocate(file.size);
-    for (uint256 i = 0; i < slices.length; i++) {
-        DynamicBuffer.appendUnchecked(
-            buffer,
-            SSTORE2.read(
-                slices[i].pointer,
-                slices[i].offset,
-                slices[i].offset + slices[i].size
-            )
-        );
-    }
+    assembly {
+        let len := mload(slices)
+        let totalSize := 0x20
+        contents := mload(0x40)
+        let sliceSize
+        let slice
+        let pointer
+        let offset
 
-    return string(buffer);
+        // loop through all slices
+        // - get pointer
+        // - get size
+        // - get offset
+        // - get code and add to contents
+        // - update total size
+
+        for {
+            let i := 0
+        } lt(i, len) {
+            i := add(i, 1)
+        } {
+            slice := mload(add(slices, add(0x20, mul(i, 0x20))))
+            pointer := mload(slice)
+            sliceSize := mload(add(slice, 0x20))
+            offset := mload(add(slice, 0x40))
+
+            extcodecopy(
+                pointer,
+                add(contents, totalSize),
+                // TODO: remove +1 once we write this as part of the slice
+                add(offset, 1),
+                sliceSize
+            )
+            totalSize := add(totalSize, sliceSize)
+        }
+
+        // update contents size
+        mstore(contents, sub(totalSize, 0x20))
+        // store contents
+        mstore(0x40, add(contents, and(add(totalSize, 0x1f), not(0x1f))))
+    }
 }
 
 using {read} for File global;
