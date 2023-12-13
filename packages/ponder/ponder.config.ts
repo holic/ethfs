96ce5f3b6ef1e1437325974d88636fb52dfba3d9
Kevin Ingersoll <kingersoll@gmail.com>
2023-12-13 11:17:10 -0800

    try a fallback rpc

--- packages/ponder/ponder.config.ts
+++ packages/ponder/ponder.config.ts
@@ -1,13 +1,16 @@
 import FileStoreAbi from "@ethfs/contracts/out/FileStore.sol/FileStore.abi.json";
 import deploys from "@ethfs/deploy/deploys.json";
 import { createConfig } from "@ponder/core";
-import { http } from "viem";
+import { fallback, http } from "viem";
 
 export default createConfig({
   networks: {
     goerli: {
       chainId: 5,
-      transport: http("https://ethereum-goerli.publicnode.com"),
+      transport: fallback([
+        // http("https://ethereum-goerli.publicnode.com"),
+        http("https://www.noderpc.xyz/rpc-goerli/public"),
+      ]),
     },
   },
   contracts: {
