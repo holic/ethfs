40ca89d39392c0aed29fdc06fee96ff11d18d773
Kevin Ingersoll <kingersoll@gmail.com>
2023-12-13 19:57:59 -0800

    use just one rpc

--- packages/ponder/ponder.config.ts
+++ packages/ponder/ponder.config.ts
@@ -1,16 +1,13 @@
 import FileStoreAbi from "@ethfs/contracts/out/FileStore.sol/FileStore.abi.json";
 import deploys from "@ethfs/deploy/deploys.json";
 import { createConfig } from "@ponder/core";
-import { fallback, http } from "viem";
+import { http } from "viem";
 
 export default createConfig({
   networks: {
     goerli: {
       chainId: 5,
-      transport: fallback([
-        // http("https://ethereum-goerli.publicnode.com"),
-        http("https://www.noderpc.xyz/rpc-goerli/public"),
-      ]),
+      transport: http("https://www.noderpc.xyz/rpc-goerli/public"),
     },
   },
   contracts: {
