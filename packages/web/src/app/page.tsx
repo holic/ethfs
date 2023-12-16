8ec71996f2d85c6d4e1a045ae56f65c33c91cf36
Kevin Ingersoll <kingersoll@gmail.com>
2023-12-16 14:27:28 -0800

    update web

--- packages/web/src/app/page.tsx
+++ packages/web/src/app/page.tsx
@@ -4,15 +4,21 @@ export default async function HomePage() {
   const files = Array.from(
     await sql`
       SELECT
-        name, created_at, size, metadata
-      FROM files
-      WHERE chain_id = 5`,
+        filename, block_time as "createdAt", size, metadata
+      FROM files_created
+      WHERE chain_id = 5
+      ORDER BY block_time DESC
+    `,
   );
+
   console.log("got files", files);
+
   return (
     <div>
       {files.map((file) => (
-        <div key={file.name}>{file.name}</div>
+        <div key={file.filename}>
+          {file.filename} {new Date(file.createdAt * 1000).toLocaleString()}
+        </div>
       ))}
     </div>
   );
