4949dbe7f4045f2947db5ac136e468c712a892d2
Kevin Ingersoll <kingersoll@gmail.com>
2023-12-29 16:25:12 +0000

    expose rpc urls to next

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,10 +1,18 @@
+// Expose RPC URLs to Next via `NEXT_PUBLIC_` prefix so we don't have to duplicate them
+const rpcUrls = Object.entries(process.env)
+  .filter(([key, value]) => /^RPC_HTTP_URL_\d+$/.test(key))
+  .map(([key, value]) => [`NEXT_PUBLIC_${key}`, value]);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    ...Object.fromEntries(rpcUrls),
+  },
   webpack: (config) => {
     // https://github.com/WalletConnect/walletconnect-monorepo/issues/1908
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
