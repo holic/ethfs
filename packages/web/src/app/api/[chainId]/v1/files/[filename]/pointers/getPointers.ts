7d6e3e9d9392eeff57a58dc8b31b637164313659
Kevin Ingersoll <kingersoll@gmail.com>
2024-01-01 23:12:58 +0000

    fix build

---
+++ packages/web/src/app/api/[chainId]/v1/files/[filename]/pointers/getPointers.ts
@@ -0,0 +1,35 @@
+"use server";
+import { createPublicClient, Hex, http } from "viem";
+import { readContract } from "viem/actions";
+
+import { supportedChains } from "../../../../../../../supportedChains";
+import { fileStoreAbi } from "./abi";
+import { fileStoreDeploys } from "./deploys";
+
+export async function getPointers(
+  chainId: number,
+  filename: string,
+): Promise<Hex[]> {
+  const deploy = fileStoreDeploys.find((d) => d.chain.id === chainId);
+  if (!deploy) {
+    throw new Error(`Chain ${chainId} not supported, no deploy`);
+  }
+  const supportedChain = supportedChains.find((c) => c.chain.id === chainId);
+  if (!supportedChain) {
+    throw new Error(`Chain ${chainId} not supported, no RPC`);
+  }
+
+  const publicClient = createPublicClient({
+    transport: http(supportedChain.rpcUrl),
+  });
+
+  // TODO: handle reverts (e.g. FileNotFound)
+  const file = await readContract(publicClient, {
+    address: deploy.address,
+    abi: fileStoreAbi,
+    functionName: "getFile",
+    args: [filename],
+  });
+
+  return file.contents.map((c) => c.pointer);
+}
